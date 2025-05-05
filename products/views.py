c9423170e69e297524f95ab6c4bbef610398f43a
Angela-Sin <anzela.sinia@gmail.com>
2025-05-05 13:33:56 +0100

    add raiting for product.html

--- products/views.py
+++ products/views.py
@@ -1,24 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category
-from django.db.models.functions import Lower
-from django.db.models import Q
-from django.urls import reverse
-from django.contrib import messages
 
 
 def all_products(request):
     products = Product.objects.all()
     categories = Category.objects.all()
 
-    # Search functionality
-    query = request.GET.get('s')  # Changed: Search query parameter 's' from the form
+    query = request.GET.get('s')
     if query:
-        products = products.filter(name__icontains=query)  # Changed: Filter products by name matching the query
+        products = products.filter(name__icontains=query)
 
-    # Category filtering functionality (if needed)
-    category_filter = request.GET.get('category')  # Changed: Category query parameter
+    category_filter = request.GET.get('category')
     if category_filter:
-        products = products.filter(category__name=category_filter)  # Changed: Filter by category
+        products = products.filter(category__name=category_filter)
 
     context = {
         'products': products,
