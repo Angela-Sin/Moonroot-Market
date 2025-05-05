82691ae8edd517308b2dcec7a21d7256312ff7e6
Angela-Sin <anzela.sinia@gmail.com>
2025-05-05 13:04:18 +0100

    add categories functionality in products and nav_bar.html

--- products/views.py
+++ products/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category
+from django.db.models.functions import Lower
+from django.db.models import Q
+from django.urls import reverse
+from django.contrib import messages
 
 
 def all_products(request):
     products = Product.objects.all()
     categories = Category.objects.all()
+
+    # Search functionality
+    query = request.GET.get('s')  # Changed: Search query parameter 's' from the form
+    if query:
+        products = products.filter(name__icontains=query)  # Changed: Filter products by name matching the query
+
+    # Category filtering functionality (if needed)
+    category_filter = request.GET.get('category')  # Changed: Category query parameter
+    if category_filter:
+        products = products.filter(category__name=category_filter)  # Changed: Filter by category
 
     context = {
         'products': products,
