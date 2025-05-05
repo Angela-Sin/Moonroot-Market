6127e5698756c3e274b3999825801d38a2c49d8b
Angela-Sin <anzela.sinia@gmail.com>
2025-05-05 15:01:17 +0100

    update product_detail.html with java functionality and add context.py

--- bag/models.py
+++ bag/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
+
 class Bag(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def total(self):
         return sum(item.subtotal() for item in self.items.all())
+
 
 class BagItem(models.Model):
     bag = models.ForeignKey(Bag, related_name='items', on_delete=models.CASCADE)
--- moonroot/settings.py
+++ moonroot/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.context.bag_contents',
             ],
         },
     },
@@ -158,6 +159,9 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STANDARD_DELIVERY_PERCENTAGE = 10
+FREE_DELIVERY_THRESHOLD = 50
 
 
 # Default primary key field type
